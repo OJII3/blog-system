41c3f5e1aa531915329e44e1fea5ce40a24d8588
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-13 11:15:48 +0900

    fix: build error

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,7 +2,7 @@ import { GetAllPostsDocument, GetPostDocument } from "@/graphql/request";
 import type { GetAllPostsQuery, GetPostQuery } from "@/graphql/types";
 import { query } from "@/lib/client";
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<{ id: string }[]> {
 	const {
 		data: {
 			folderFromPath: { notes },
@@ -16,24 +16,28 @@ export async function generateStaticParams() {
 	});
 	await new Promise((resolve) => setTimeout(resolve, 100));
 
-	if (error) {
+	if (error || !notes.edges?.length) {
 		throw error;
 	}
 
-	return notes.edges
-		?.map((edge) => ({
-			id: edge?.node?.id,
-		}))
-		.filter((id) => Boolean(id));
+	return notes.edges.flatMap((edge) => {
+		if (!edge?.node?.id) return [];
+		return { id: edge.node.id };
+	});
 }
 
-export default async function Blog({ params }: { params: { id: string } }) {
-	const { id } = params;
+export default async function Blog({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
 
 	const { data, error } = await query<GetPostQuery>({
 		query: GetPostDocument,
 		variables: { id },
 	});
+	await new Promise((resolve) => setTimeout(resolve, 100));
 
 	if (error) {
 		throw error;
